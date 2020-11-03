9cc0faaecd958bffb3cd8aa92f8699014a4f57c0
Cameron Bisca <cjbisca@gmail.com>
2020-11-03 21:23:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
   count = 0 
   new_array = []
+  
   while count < array.length do 
    new_array.push(Math.sqrt(array[count]))
     count += 1 
   end
- new_array 
+  new_array 
 end
